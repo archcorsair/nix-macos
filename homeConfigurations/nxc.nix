8f6ceb0d3979729d0a5da363558b85c0fc178cb8
Daniel Shneyder <archcorsair@gmail.com>
2024-10-05 12:18:22 -0700

    fix: typo

--- homeConfigurations/nxc.nix
+++ homeConfigurations/nxc.nix
@@ -317,7 +317,7 @@
           whatismyip = "curl -s 'https://api.ipify.org?format=json' | jq -r '.ip'";
           wimi = "whatismyip";
           vim = "nvim";
-          rust-env = "~/.config/nix/rust-env.sh"
+          rust-env = "~/.config/nix/rust-env.sh";
         };
         syntaxHighlighting = {
           enable = true;
