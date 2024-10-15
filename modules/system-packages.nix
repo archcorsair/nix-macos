a2e297be3329f34f37399a1b3eced85803e3c6b7
Daniel Shneyder <archcorsair@gmail.com>
2024-10-14 22:55:20 -0700

    feat: Add very important packages

--- modules/system-packages.nix
+++ modules/system-packages.nix
@@ -4,14 +4,17 @@
   environment.systemPackages = with pkgs; [
     bun
     commitizen
+    cowsay
     curlie
     delta
     doggo
     fd
     fnm
+    fortune
     ghq
     httpie
     jq
+    lolcat
     mas
     neofetch
     neovim
@@ -19,6 +22,7 @@
     nixfmt-rfc-style
     pnpm
     ripgrep
+    sl
     speedtest-cli
     termshark
   ];
