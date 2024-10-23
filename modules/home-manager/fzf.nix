b9a33af0339412b4bc27664b7d27f13147205485
Daniel Shneyder <archcorsair@gmail.com>
2024-10-23 13:37:54 -0700

    feat: Update fzf config

--- modules/home-manager/fzf.nix
+++ modules/home-manager/fzf.nix
@@ -3,7 +3,7 @@
     enable = true;
     defaultCommand = "fd --type file --color=always";
     defaultOptions = [
-      "--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid --line-range :300 {}'"
+      "--ansi --preview-window 'right:60%' --preview 'bat --color=always --style=header,grid,numbers --line-range :500 {}'"
     ];
   };
 }
