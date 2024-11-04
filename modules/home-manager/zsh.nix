c567a60dea981a122eb0b9e7e8bd8c2a47136500
Daniel Shneyder <archcorsair@gmail.com>
2024-11-04 11:10:25 -0800

    format: add cd to update command

--- modules/home-manager/zsh.nix
+++ modules/home-manager/zsh.nix
@@ -27,7 +27,7 @@
       man = "batman";
       nix-shell = "nix-shell --run $SHELL";
       tree = ''br -c :pt "$@"'';
-      update = "nix flake update && darwin-rebuild switch --flake ~/ghq/github.com/archcorsair/nix-macos#mbp --show-trace";
+      update = "cd nix-macos && nix flake update && darwin-rebuild switch --flake ~/ghq/github.com/archcorsair/nix-macos#mbp --show-trace";
       whatismyip = "curl -s 'https://api.ipify.org?format=json' | jq -r '.ip'";
       wimi = "whatismyip";
     };
