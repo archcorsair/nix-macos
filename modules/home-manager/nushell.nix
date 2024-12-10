46c24c17a71eeb57d46359f14b160e6cc5fdb77a
Daniel Shneyder <archcorsair@gmail.com>
2024-12-10 11:01:13 -0800

    feat: Improve nushell config

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -26,26 +26,49 @@
     '';
 
     configFile.text = ''
-      $env.config = {
-        show_banner: false
-      }
+        $env.config = {
+          show_banner: false
+        }
 
-      # Starhip Prompt
-      use ~/.cache/starship/init.nu
+        # Starhip Prompt
+        use ~/.cache/starship/init.nu
 
-      # Zoxide
-      source ~/.zoxide.nu
+        # Zoxide
+        source ~/.zoxide.nu
 
-      # whatismyip
-      def whatismyip [] {
-        http get https://api.ipify.org?format=json | get ip
-      }
+        # update
+        def rebuild [] {
+          cd ~/ghq/github.com/archcorsair/nix-macos
+          nix flake update
+          darwin-rebuild switch --flake ~/ghq/github.com/archcorsair/nix-macos#mbp --show-trace
+        }
 
-      # update
-      def _update [] {
-        cd ~/ghq/github.com/archcorsair/nix-macos
-        nix flake update
-        darwin-rebuild switch --flake ~/ghq/github.com/archcorsair/nix-macos#mbp --show-trace
+        def "version-completions" [] {
+        [
+          {value: "4", description: "IPv4 address (default)"},
+          {value: "6", description: "IPv6 address"}
+        ]}
+
+        # Define the function with completion annotation
+        def "whatismyip" [
+          version?: int@version-completions # Parameter with custom completion
+        ] {
+          # Validate and set default version
+          let version = if ($version == null) {
+            4
+          } else if ($version in [4 6]) {
+            $version
+          } else {
+            error make {
+            msg: "Invalid IP version. Must be either 4 or 6."
+          }
+        }
+        # Get IP address
+        let ip = (http get $"https://api($version).ipify.org")
+        # Create record and display as table
+        {
+          $"ip($version)": $ip
+        } | table
       }
     '';
   };
