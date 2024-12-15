fac98f5afad647c895ebf14bd1731417f192e11c
Daniel Shneyder <archcorsair@gmail.com>
2024-12-15 13:08:03 -0800

    bug: Temporarily disable fnm config (not working as intended)

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -14,6 +14,15 @@
     };
 
     envFile.text = ''
+      # FNM
+      # fnm env --json | from json | load-env
+      # use std "path add"
+      # $env.FNM_BIN = $"($env.FNM_DIR)/bin"
+      # path add $env.FNM_BIN
+      # $env.FNM_MULTISHELL_PATH = $"($env.FNM_DIR)/nodejs"
+      # path add $env.FNM_MULTISHELL_PATH
+
+
       # Environment Variables
       $env.XDG_CONFIG_HOME = ($env.HOME | path join ".config")
       $env.XDG_DATA_HOME = ($env.HOME | path join ".local/share")
@@ -39,14 +48,6 @@
         $env.config = {
           show_banner: false
         }
-
-        # FNM
-        fnm env --json | from json | load-env
-        use std "path add"
-        $env.FNM_BIN = $"($env.FNM_DIR)/bin"
-        path add $env.FNM_BIN
-        $env.FNM_MULTISHELL_PATH = $"($env.FNM_DIR)/nodejs"
-        path add $env.FNM_MULTISHELL_PATH
 
         # update
         def rebuild [] {
