2a37c1fa059cf69dd3c1cc904b7d95c9d3f507a5
Daniel Shneyder <archcorsair@gmail.com>
2024-12-15 10:29:22 -0800

    feat: Add FNM config

--- modules/home-manager/nushell.nix
+++ modules/home-manager/nushell.nix
@@ -40,6 +40,14 @@
           show_banner: false
         }
 
+        # FNM
+        fnm env --json | from json | load-env
+        use std "path add"
+        $env.FNM_BIN = $"($env.FNM_DIR)/bin"
+        path add $env.FNM_BIN
+        $env.FNM_MULTISHELL_PATH = $"($env.FNM_DIR)/nodejs"
+        path add $env.FNM_MULTISHELL_PATH
+
         # update
         def rebuild [] {
           cd ~/ghq/github.com/archcorsair/nix-macos
